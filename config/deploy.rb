8111c3944fcaf1051a2df2d7c2595406b554d977
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-11-06 15:40:27 +0100

    Added capistrano stuff.

---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+# Set the application name
+set :application, 'fjarrkontrollen-server'
+
+# Set the repository link
+set :repo_url, 'https://github.com/ub-digit/fjarrkontrollen-server.git'
+
+# Set tmp directory on remote host - Default value: '/tmp , which often will not allow files to be executed
+set :tmp_dir, '/home/apps/tmp'
+
+# Copy originals into /{app}/shared/config from respective sample file
+set :linked_files, %w{config/database.yml config/initializers/illbackend_cfg.rb db/seeds/locations.rb db/seeds/testdata.rb db/seeds/users.rb}
+
+set :rvm_ruby_version, "2.1.1"              # use the same ruby as used locally for deployment
