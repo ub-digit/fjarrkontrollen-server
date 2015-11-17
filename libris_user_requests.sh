d0974c306dae9fbdbcb64b23ccadca60f45a24e8
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-11-17 15:33:56 +0100

    Changed paths in the cron scripts to reflect the new paths in use.

--- libris_user_requests.sh
+++ libris_user_requests.sh
@@ -4,5 +4,5 @@
 # is run by cronjob fjarrkontrollen_server_ENV_cron_script
 
 CURRENT_ENV=$1
-cd /apps/fjarrlan/fjarrkontrollen-server
+cd /apps/fjarrlan/fjarrkontrollen-server/current
 RAILS_ENV=${CURRENT_ENV} /usr/local/rvm/gems/ruby-2.1.5/wrappers/rake libris_info:user_requests
