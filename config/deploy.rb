ffd87216bc47a9f3bb22ba0de742c40759b9b299
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2017-05-02 16:13:40 +0200

    adding relevant config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,14 +7,19 @@ set :application, 'fjarrkontrollen-server'
 # Set the repository link
 set :repo_url, 'https://github.com/ub-digit/fjarrkontrollen-server.git'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-set :deploy_to, '/apps/fjarrlan/fjarrkontrollen-server'
-
 # Set tmp directory on remote host - Default value: '/tmp , which often will not allow files to be executed
 set :tmp_dir, '/home/apps/tmp'
 
 # Copy originals into /{app}/shared/config from respective sample file
-#set :linked_files, %w{config/database.yml config/initializers/illbackend_cfg.rb config/initializers/secret_token.rb db/seeds/locations.rb db/seeds/testdata.rb db/seeds/users.rb}
+set :linked_files, %w{config/database.yml config/initializers/illbackend_cfg.rb config/initializers/secret_token.rb db/seeds/locations.rb db/seeds/testdata.rb db/seeds/users.rb}
 
-set :rvm_ruby_version, "2.1.5"              # use the same ruby as used locally for deployment
+set :rvm_ruby_version, '2.1.5'      # Defaults to: 'default'
+
+# Returns config for current stage assigned in config/deploy.yml
+def deploy_config
+  @config ||= YAML.load_file("config/deploy.yml")
+  stage = fetch(:stage)
+  return @config[stage.to_s]
+end
+
+server deploy_config['host'], user: deploy_config['user'], roles: deploy_config['roles']
