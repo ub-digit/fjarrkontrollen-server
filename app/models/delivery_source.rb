4f445e315258b1660b75f21946ee78d04d1b87fe
David Gustafsson <david.gustafsson@ub.gu.se>
2023-12-20 19:50:57 +0100

    Use oauth2 authentication

--- app/models/delivery_source.rb
+++ app/models/delivery_source.rb
@@ -3,7 +3,7 @@ class DeliverySource < ApplicationRecord
 
   validates_presence_of :label
   validates_uniqueness_of :label
-  
+
   def as_json(options = {})
     super(:except => [:created_at, :updated_at])
   end
