84d4ff7632892ad19035280fe51905dfd23205ea
David  Gustafsson <david.gustafsson@ub.gu.se>
2024-06-12 15:58:03 +0200

    Remove obsolete authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,22 +13,6 @@ class User < ApplicationRecord
   validates :xkonto, :name, presence: true
   validates_uniqueness_of :xkonto
 
-  def authenticate(provided_password)
-    uri = URI('https://login-server.ub.gu.se/auth/' + xkonto)
-    params = { :password => provided_password}
-    params[:service] = 'test' if Rails.env == 'test'
-    uri.query = URI.encode_www_form(params)
-    res = Net::HTTP.get_response(uri)
-    json_response = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
-    #puts json_response["auth"]["yesno"]
-
-    if(json_response["auth"]["yesno"])
-      token_object = generate_token
-      return token_object.token
-    end
-    false
-  end
-
   def generate_token
     access_tokens.create(token: SecureRandom.hex, token_expire: Time.now + DEFAULT_TOKEN_EXPIRE)
   end
